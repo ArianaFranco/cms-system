edc51f7198867c41eae51d8d18da67cfd2a6d75c
Ariana Franco <ariana.francof@gmail.com>
2020-10-09 11:14:25 -0500

    Using Factories tu use dummy values for users and posts.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    
+    //If you would like to make all attributes mass assignable, you may define the $guarded property as an empty array:
+    //protected $guarded = [];
+    
+    protected $fillable = [
+        'user_id',
+        'title',
+        'post_image',
+        'body',
+    ];
+    
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
