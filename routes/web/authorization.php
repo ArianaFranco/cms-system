fc40cfc394bf40e1ec3b4ce3565477fc98f75d2b
Ariana Franco <ariana.francof@gmail.com>
2020-10-16 16:07:36 -0500

    Updating RouteServiceProvider for using a file for authorization routes.

---
+++ routes/web/authorization.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::middleware('auth')->group(function () {
+    Route::middleware('role:admin')->group(function () {
+        
+        Route::resource('roles', App\Http\Controllers\RoleController::class);
+        Route::resource('permissions', App\Http\Controllers\PermissionController::class);
+        
+    });
+});
+
+
+
